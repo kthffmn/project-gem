1bd18bc3fc4f2f28b05f5c0b328f3a9bea91dffa
Katie Hoffman <ktahoffman@gmail.com>
2016-04-04 13:55:28 -0400

    fix java testing error

--- lib/helpers/template-helper.rb
+++ lib/helpers/template-helper.rb
@@ -16,6 +16,6 @@ module TemplateHelper
     edit_file('src/main/java/ClassName.java', { class_name: camel_case_class_name })
     edit_file('src/test/java/ClassNameTest.java', { class_name: camel_case_class_name })
     change_filename('src/test/java/', 'ClassNameTest.java', 'java', "#{camel_case_class_name}Test")
-    change_filename('src/main/java/ClassName.java', 'ClassName.java', 'java', "#{camel_case_class_name}")
+    change_filename('src/main/java/', 'ClassName.java', 'java', "#{camel_case_class_name}")
   end
 end
